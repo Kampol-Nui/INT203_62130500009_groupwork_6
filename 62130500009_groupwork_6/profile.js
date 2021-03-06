13debed73d1a417d82dc925723eb2b4c2b997686
Kampol Thaipricha <kampol.43@mail.kmutt.ac.th>
2021-03-06 10:36:09 +0700

    clean comment

--- 62130500009_groupwork_6/profile.js
+++ 62130500009_groupwork_6/profile.js
@@ -1,15 +1,8 @@
     const app = Vue.createApp({
         data() {
             return {
-                // myname: 'Kampol Thaipricha',
                 submitdata: {
                     mypic: 'images/2.jpg',
-                    // member: 'Member since Jan 2021',
-                    // rawHtml1: '<span class="font-weight-bold">Post</span>',
-                    // rawHtml2: '<span class="font-weight-bold">Comments</span>',
-                    // rawHtml3: '<span class="font-weight-bold">Favourites</span>',
-                    // follow:'follow',
-                    // num:[281,2,37,12],
                     firstname: null,
                     lastname: null,
                     password: null,
@@ -78,23 +71,9 @@
     const constraints = {
         firstname: {
             presence: true,
-            // exclusion: {
-            //     within: {
-            //         username: "Japan",
-            //         "ch": "China"
-            //     },
-            //     message: "^We don't support %{value} right now, sorry"
-            // }
         },
         lastname: {
             presence: true,
-            // exclusion: {
-            //     within: {
-            //         username: "Japan",
-            //         "ch": "China"
-            //     },
-            //     message: "^We don't support %{value} right now, sorry"
-            // }
         },
         email: {
             presence: true,
@@ -138,26 +117,6 @@
             }
 
         }
-        // age: {
-        //     presence: true,
-        //     numericality: {
-        //         lessThan: 150
-        //     }
-        // },
-        // gender: {
-        //     presence: true,
-        // },
-
-        // phone: {
-        //     presence: true,
-        //     length: {
-        //         minimum : 10,
-        //         message: "must be at least 10 digits"
-        //     },        
-        // },
-        // chosensubject: {
-        //     presence: true
-        // }
     }
 
 
